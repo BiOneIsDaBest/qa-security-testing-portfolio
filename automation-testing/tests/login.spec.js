cc6762b9694e1001408cf5862114cab173505503
BiOne_HLT <nguyentunganhbao.au@gmail.com>
2026-06-09 13:36:07 +1000

    Clean up Playwright login tests

--- automation-testing/tests/login.spec.js
+++ automation-testing/tests/login.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.describe('SauceDemo Automation Tests', () => {
+test.describe('SauceDemo Login Tests', () => {
   async function login(page, username = 'standard_user', password = 'secret_sauce') {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('#user-name').fill(username);
@@ -20,26 +20,5 @@ test.describe('SauceDemo Automation Tests', () => {
 
     await expect(page.locator('[data-test="error"]')).toBeVisible();
     await expect(page.locator('[data-test="error"]')).toContainText('Username and password do not match');
-  });
-
-  test('TC-AUTO-CART-001 - User should be able to add product to cart', async ({ page }) => {
-    await login(page);
-
-    await page.locator('#add-to-cart-sauce-labs-backpack').click();
-
-    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
-    await expect(page.locator('#remove-sauce-labs-backpack')).toBeVisible();
-  });
-
-  test('TC-AUTO-CART-002 - User should be able to remove product from cart', async ({ page }) => {
-    await login(page);
-
-    await page.locator('#add-to-cart-sauce-labs-backpack').click();
-    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
-
-    await page.locator('#remove-sauce-labs-backpack').click();
-
-    await expect(page.locator('.shopping_cart_badge')).toHaveCount(0);
-    await expect(page.locator('#add-to-cart-sauce-labs-backpack')).toBeVisible();
   });
 });
